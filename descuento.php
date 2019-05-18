9c1ca2a84f8a8af6a09b53ee2e9ec76587a37162
brayan2270 <50777728+brayan2270@users.noreply.github.com>
2019-05-18 15:49:24 -0500

    Add files via upload

---
+++ descuento.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>OFERTAS</title>
+	<meta charset="utf-8">
+	
+	<link rel="stylesheet" href="css/estilo.css">
+
+
+</head>
+	
+	</div>
+
+<body>
+
+	
+	<nav class="navegacion">
+		<ul class="menu">
+
+			<li class="first-item">
+				<a href="./index.php">
+					<img class="hola" src="./img/menu.jpg" alt="" class="imagen">
+					<span class="text-item">Inicio</span>
+					<span class="down-item"></span>
+				</a>
+			</li>
+
+			<li>
+				<a href="./fruteria.php">
+					<img src="./img/frutas.jpg" alt="" class="imagen">
+					<span class="text-item">Frutas</span>
+					<span class="down-item"></span>
+				</a>
+			</li>
+
+			<li>
+				<a href="./verduras.php">
+					<img src="./img/verduras.jpg" alt="" class="imagen">
+					<span class="text-item">Verduras</span>
+					<span class="down-item"></span>
+				</a>
+			</li>
+
+			<li>
+				<a href="./descuento.php">
+					<img src="./img/oferta.jpg" alt="" class="imagen">
+					<span class="text-item">Ofertas</span>
+					<span class="down-item"></span>
+				</a>
+			</li>
+
+			<li>
+				<a href="./contacto.php">
+					<img src="./img/contacto.jpg" alt="" class="imagen">
+					<span class="text-item">Contactanos</span>
+					<span class="down-item"></span>
+				</a>
+            </li>
+            
+           
+
+        </ul>
+	</nav>
+	<div id="lolo">
+    <div class="cuerpo">
+	<h1  class="of">OFERTAS</h1>
+
+
+	<h2>Ofertas en verduras desde el 12 hasta el 29 de mayo; toda verdura que encuentres estara en oferta. Aprovecha hasta el maximo esta oportunidad.</h2><img src="img/verduras.jpg">
+</br></br>
+
+	<h2 ><b>¿Te gustan las manzanas?</b> aprovecha, este fin de semana podras llevar 2  manzanas màs por la compra de 3 no la dejes pasar.</h2><img src="img/manz.jpg"></br></br>
+
+	<h2><b>35% de descuento en frutas y verduras por la compra de mas de 50.000 COP.</h2><img src="img/oferta.jpg"></br></br>
+
+
+	
+</div>
+</div>
+</body>
+</html>
